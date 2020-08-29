d419df7394bb9fcf9b8b711a62c225211bdbd08b
kurooooo <kurooooo@icloud.com>
2020-08-30 05:04:53 +0800

    Site updated: 2020-08-30 05:04:53

--- uploads/codes/w6/test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("%4$d %1$d %3$d %2$d\n", 1, 2, 3, 4);
-	return 0;
-}
--- uploads/codes/w8/a.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-	int x;
-	scanf("%d", &x);
-	printf("%d\n", x);
-	return 0;
-}
